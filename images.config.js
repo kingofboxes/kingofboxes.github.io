f76d48b9203ffc2d2ca3e961ea4d6c39bea01f30
kingofboxes <jliang1641@gmail.com>
2021-01-14 20:43:18 +1100

    Fixed next-optimized-images config file

--- images.config.js
+++ images.config.js
@@ -1,5 +1,5 @@
 // next-optimized-images settings.
 module.exports = {
-  default: { webp: false },
+  default: { webp: true },
   types: { thumbnail: { sizes: [200, 400], breakpoints: [800], webp: false } },
 };
