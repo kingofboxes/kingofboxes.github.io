f516c63ee57aaca347ff9da6b3268b4768dfb62f
kingofboxes <jliang1641@gmail.com>
2021-01-18 17:22:04 +1100

    Finished Maimai DX song list

---
+++ components/public/Maimai/Maimai.constants.ts
@@ -0,0 +1,68 @@
+/* eslint-disable prettier/prettier */
+import { MMSongRecord, MMSongData } from '../../../types';
+
+// Genre list for maimai.
+export const getGenres = (list: MMSongRecord[]): Record<string, string>[] => {
+  const genres: string[] = [];
+  const genreList: Record<string, string>[] = [];
+
+  // Map out dropdown from list of unique genres.
+  list.forEach((song: MMSongRecord) => {
+    if (!genres.includes(song.genre)) {
+      genres.push(song.genre);
+    }
+  });
+
+  genres.map((genre: string) => {
+    genreList.push({
+      key: genre,
+      text: genre,
+      value: genre,
+    });
+  });
+  return genreList;
+};
+
+// Level list for maimai.
+export const getLevels = (list: MMSongRecord[]): Record<string, string>[] => {
+  const levels: string[] = [];
+  const strLevels: string[] = [];
+  const floatLevels: number[] = [];
+  const levelsList: Record<string, string>[] = [];
+
+  // Get a list of unique levels and turn any 'X+' into 'X.5's.
+  list.forEach((song: MMSongRecord) => {
+    Object.keys(song.data).map((diff: string) => {
+      if (!levels.includes(song.data[diff as keyof MMSongData].level)) {
+        const temp = song.data[diff as keyof MMSongData].level.replace('+', '.5');
+        floatLevels.push(Number(temp));
+        levels.push(song.data[diff as keyof MMSongData].level);
+      }
+    });
+  });
+
+  // Sort the new array of floats, then turn all of them back into standard levels.
+  floatLevels.sort((a, b) => a - b);
+  floatLevels.forEach((level) => {
+    const temp = String(level).replace('.5', '+');
+    strLevels.push(temp);
+  });
+
+  // Map out dropdown from list of levels.
+  strLevels.map((level: string) => {
+    levelsList.push({
+      key: level,
+      text: level,
+      value: level,
+    });
+  });
+  return levelsList;
+};
+
+// Default options.
+export const defaultOptions: Record<string, string> = {
+  name: '',
+  artist: '',
+  level: '',
+  genre: '',
+};
--- components/public/Maimai/Maimai.tsx
+++ components/public/Maimai/Maimai.tsx
@@ -2,11 +2,15 @@
 import React from 'react';
 import { NextPage } from 'next';
 import axios, { AxiosInstance } from 'axios';
+import { DropdownProps, Form } from 'semantic-ui-react'
 
 import ContentHeader from '../../common/ContentHeader';
 import PublicShell from '../../common/PublicShell';
-import { MMPlayerData, MMSongRecord, ContentProps } from '../../../types';
+import { MMPlayerData, MMSongRecord, ContentProps, MMSongData } from '../../../types';
 import { MaimaiSongComponent, MaimaiSongLoader } from './Maimai.data';
+import { getGenres, getLevels, defaultOptions } from './Maimai.constants';
+
+import styles from '../../styling/Maimai.module.css';
 
 // Create Axios instance.
 const api: AxiosInstance = axios.create({
@@ -31,18 +35,22 @@ const contentProps: ContentProps = {
 };
 
 const MaimaiDX: NextPage<MMPlayerData> = (data) => {
+
+  // Variables for controlling state on this page.
   const [loading, setLoading] = React.useState<boolean>(false);
   const [results, setResults] = React.useState<number>(5);
+  const [list, setList] = React.useState<MMSongRecord[]>(data.record);
+  const [query, setQuery] = React.useState<Record<string, string>>(defaultOptions);
+  const genreList = getGenres(data.record);
+  const levelsList = getLevels(data.record);
 
   // UseEffect hook for infinite scroll.
   React.useEffect(() => {
-    // Function to check whether the scroll bar is at the bottom.
-    // If it is at the bottom, create a "loading" menu and display 10 more results after 0.5s.
     const checkScroll = () => {
       const height = document.body.scrollHeight;
       const curr = window.scrollY + window.innerHeight;
       if (curr >= height) {
-        if (results < data.record.length) {
+        if (list && results < list.length) {
           setLoading(true);
           setTimeout(() => {
             setLoading(false);
@@ -51,22 +59,97 @@ const MaimaiDX: NextPage<MMPlayerData> = (data) => {
         }
       }
     };
-
-    // Add event listener when mounting component.
     window.addEventListener('scroll', checkScroll);
-
-    // Remove event listener when unmounting component.
     return () => {
       window.removeEventListener('scroll', checkScroll);
     };
-  }, [results]);
+  }, [list, results]);
+
+  // Handle filtering by name, artist, genre and level.
+  const setName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const options = {...query, name: event.target.value}
+    filter(options)
+  }
+
+  const setArtist = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const options = {...query, artist: event.target.value}
+    filter(options)
+  }
+
+  const setGenre = (_event: React.SyntheticEvent<HTMLElement>, data: DropdownProps) => {
+    const genre = String(data.value);
+    const options: Record<string, string> = {...query, genre: genre}
+    filter(options)
+  }
+
+  const setLevel = (_event: React.SyntheticEvent<HTMLElement>, data: DropdownProps) => {
+    const level = String(data.value);
+    const options: Record<string, string> = {...query, level: level}
+    filter(options)
+  }
+
+  const checkLevel = (list: MMSongRecord[], level: string) => {
+    // Take care of meta character for regex.
+    const levelPattern = level.replace('+', `\\+`);
+    let newList: MMSongRecord[] = [];
+    const pattern = new RegExp(`^${levelPattern}$`, 'i');
+    list.forEach((song: MMSongRecord) => {
+      Object.keys(song.data).map((diff: string) => {
+        if (pattern.test(song.data[diff as keyof MMSongData].level)) {
+          newList.push(song);
+        }
+      })
+    })
+
+    // Filter out duplicate songs with same levels for multiple difficulties.
+    newList = newList.filter((song, idx) => newList.indexOf(song) === idx);
+    return newList;
+  }
+
+  const filter = (options: Record<string, string>) => {
+
+    let pattern: RegExp;
+    let newList = data.record;
+
+    if (options.name !== '') {
+      pattern = new RegExp(options.name, 'i');
+      newList = newList.filter((song) => pattern.test(song.song));
+    }
+
+    if (options.artist !== '') {
+      pattern = new RegExp(options.artist, 'i');
+      newList = newList.filter((song) => pattern.test(song.artist));
+    }
+
+    if (options.genre !== '') {
+      pattern = new RegExp(options.genre, 'i');
+      newList = newList.filter((song) => pattern.test(song.genre));
+    }
+
+    if (options.level !== '') {
+      newList = checkLevel(newList, options.level);
+    }
+
+    setList(newList);
+    setQuery(options);
+  }
 
   return (
     <PublicShell title="Maimai DX Song List | Justin's Website">
       <ContentHeader data={contentProps} />
       <h2>Song List</h2>
-      {data.record &&
-        data.record.map((song: MMSongRecord, idx: number) => {
+      <div className={styles.search}>
+        <Form inverted>
+          <Form.Group widths='equal'>
+            <Form.Input fluid label='Song Name:' placeholder='Search by song name...' value={query.name} onChange={setName} />
+            <Form.Input fluid label='Artist:' placeholder='Search by artist...' value={query.artist} onChange={setArtist}/>
+            <Form.Dropdown fluid label='Genre:' selection placeholder='Filter by genre...' options={genreList} onChange={setGenre} />
+            <Form.Dropdown fluid label='Level:' selection placeholder='Filter by level...' options={levelsList} onChange={setLevel} />
+          </Form.Group>
+        </Form>
+      </div>
+      {list &&
+        list.map((song: MMSongRecord, idx: number) => {
           if (idx < results) {
             return <MaimaiSongComponent song={song} key={song.id} />;
           }
